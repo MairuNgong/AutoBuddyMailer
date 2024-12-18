6d57c0b609394fade00d106b9c32d32016816c9e
jp <ssupawej@gmail.com>
2024-12-18 18:33:56 +0700

    AddDockerLocal

--- backend/app.py
+++ backend/app.py
@@ -51,8 +51,10 @@ def assign_buddies(participants):
 
 
 origins = [
-    "http://127.0.0.1:5500", 
-    "http://127.0.0.1:8000", 
+    "http://localhost:8080",  
+    "http://127.0.0.1:8080",  
+    "http://frontend:8080",  
+    "http://backend:8000",  
 ]
 
 app.add_middleware(
--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:8000/sent-emails"
+const url = "http://localhost:5000/sent-emails"
 
 const AddItem=(event)=>{
 
